6b7c5ab2094787d37be0de1990700c35d4b3bc6a
farukh <farukh022198@gmail.com>
2023-08-01 21:58:37 -0400

    implemented select option changed css for quiz

--- quizzical/src/components/Answers.js
+++ quizzical/src/components/Answers.js
@@ -2,9 +2,15 @@ import React from "react";
 
 export default function Answers(props){
 
+    const styles = {
+        backgroundColor: props.isHeld ? "#D6DBF5" : "white"
+    }
     return(
-        <div>
-            <button>{props.value}</button>
+        <div className="allAnswers"
+            style={styles}
+            onClick={props.holdAnswer}
+        >    
+            <div className="answers-value">{props.value}</div>
         </div>
     )
 }
--- quizzical/src/components/Quiz.js
+++ quizzical/src/components/Quiz.js
@@ -2,27 +2,68 @@ import React from "react";
 import Answers from "./Answers.js"
 export default function Quiz(props){
     const data = props.triviaData;
-    console.log(props.triviaData); 
-    //<h1 className="text">{props.data.results[0].question}</h1>
-    //const question = data.map(element =>(<div key={element.question.id}>{element.question.value}</div>))
+    //console.log(props.triviaData); 
 
-    const allAnswers =  data.map((item, index) => (
+    const [allAnswers, setAllAnswers] = React.useState(data.map(item => item.allAnswers));
+    //console.log(props.triviaData); 
+    console.log(allAnswers); 
+    
+  /*
+    const quiz =  data.map((item, index) => (
           <div className="container" key={index}>
             <div className="questions">{item.question.value}</div>
             {item.allAnswers.map((element, innerIndex) => (
-              //<div className="answers" key={innerIndex}>{element.value}</div>
-            <Answers value={element.value} 
+            <Answers 
+                     key = {element.id}
+                     value={element.value} 
                      isHeld={element.isHeld}
                      id={element.id}
                      />    
             ))}
           </div>
         ));
-      
+  */   
+  function quiz(data){
+    const quiz = [];
+    for (let index = 0; index < data.length; index++) {
+      const item = data[index];
+      const answers = [];
 
+      for (let innerIndex = 0; innerIndex < item.allAnswers.length; innerIndex++) {
+        const element = allAnswers[index][innerIndex];
+        answers.push(
+          <Answers
+            key={element.id}
+            value={element.value}
+            isHeld={element.isHeld}
+            holdAnswer={() => holdAnswer(element.id)}
+          />
+        );
+      }
+
+      function holdAnswer(id){
+        setAllAnswers(oldAllAnswers => oldAllAnswers.map(outerLoop =>(
+          outerLoop.map(answer => {
+            return answer.id === id ?
+                   {...answer, isHeld: !answer.isHeld}:
+                   answer
+          })
+        )))
+      }
+
+      quiz.push(
+        <div className="container" key={index}>
+          <div className="questions">{item.question.value}</div>
+          <div className="allAnswersContainer">{answers}</div>
+        </div>
+      );
+    }
+    return quiz; 
+  }
+          
     return(
         <div>
-            {allAnswers} 
+            {quiz(data)} 
             <div className="TopRightShape"></div>
             <div className="ButtonLeftShape"></div>
         </div> 
