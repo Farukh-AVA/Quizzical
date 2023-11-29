46d00217f2633d46dc494855ecf2b56faefec80f
farukh <farukh022198@gmail.com>
2023-11-28 21:13:27 -0500

    changed welcome page

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -11,7 +11,7 @@ export default function StartQuiz(props){
     return(
         <div> 
           <h1 className="title">Quizzical</h1>
-          <p className="description">Some description if needed</p>
+          <p className="description">Welcome to the Geography Quiz Game!</p>
           <button onClick={startQuiz} className="start-quiz-button">Start quiz</button>
           <div className="TopRightShape"></div>
           <div className="ButtonLeftShape"></div>
